81e46d355496fa2a79390be22ae2e5a2ad245b58
EdwAch <267367592+EdwAch@users.noreply.github.com>
2026-04-05 17:50:33 +0300

    Allowed multiple doors for one button and button animations

--- ButtonController.cs
+++ ButtonController.cs
@@ -2,9 +2,13 @@ using Godot;
 
 public partial class ButtonController : Area2D {
     
-    [Export] private PuzzleDoor puzzleDoor;
+    [Export] private PuzzleDoor[] puzzleDoor;
+    [Export] private MeshInstance2D buttonMeshInstance;
+
+    private Vector2 buttonMovementOnPress = new Vector2(0f, 2f);
     public override void _Ready() {
         BodyEntered += OnBodyEntered;
+        BodyExited += OnBodyExited;
     }
 
     private void OnBodyEntered(Node2D body) {
@@ -13,7 +17,20 @@ public partial class ButtonController : Area2D {
         }
     }
 
+    private void OnBodyExited(Node2D body) {
+        if (body is PlayerController playerController) {
+            ButtonReleased();
+        }
+    }
+
     private void ButtonPressed() {
-        puzzleDoor.ToggleDoorState();
+        buttonMeshInstance.Translate(buttonMovementOnPress);
+        foreach (PuzzleDoor puzzleDoor in puzzleDoor) {
+            puzzleDoor.ToggleDoorState();    
+        } 
+    }
+
+    private void ButtonReleased() {
+        buttonMeshInstance.Translate(-buttonMovementOnPress);
     }
 }
