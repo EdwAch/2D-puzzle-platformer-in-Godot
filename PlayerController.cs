300ed58b640e5d1f42a45aab0188cfc6d917c29f
EdwAch <267367592+EdwAch@users.noreply.github.com>
2026-04-08 17:21:43 +0300

    double jump

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,6 +8,7 @@ public partial class PlayerController : CharacterBody2D
 	[Export] private float jumpSpeed = -200f;
 	[Export] private ShapeCast2D groundChecker;
 
+	private bool doubleJump = false;
 	private float friction = 35f;
     // 35 is good for normal ground, less is more time to reach 0
 
@@ -25,18 +26,23 @@ public partial class PlayerController : CharacterBody2D
 
 		if (direction !=0) {
 			currentVelocity.X = direction * speed;
-		} else if (IsGrounded()) {
+		} else if (IsGrounded() && currentVelocity.X != 0) {
 			currentVelocity.X = Mathf.MoveToward(Velocity.X, 0, friction);
-		}
+		} /*else if (!IsGrounded() && currentVelocity.X != 0) {
+			currentVelocity.X = Mathf.MoveToward(Velocity.X, 0, 10000f);
+		} Uncomment this if dont want movement when no movement key is pressed and midair
+		*/
 
 		if (IsGrounded() && Input.IsActionJustPressed("Jump")) {
 			currentVelocity.Y = jumpSpeed;
+			doubleJump = !doubleJump;
 		}
-
-		if (Input.IsActionJustReleased("Jump")) {
-			if (currentVelocity.Y < 0) {
+		if (Input.IsActionJustReleased("Jump") && currentVelocity.Y < 0) {
 				currentVelocity.Y = currentVelocity.Y * 0.5f;
-			}
+		}
+		if (!IsGrounded() && Input.IsActionJustPressed("Jump") && doubleJump) {
+			currentVelocity.Y = jumpSpeed;
+			doubleJump = !doubleJump;
 		}
 		
 		Velocity = currentVelocity;
